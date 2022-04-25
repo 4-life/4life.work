ea1b8f6d3596648fa963fc6867b1dc1fd0082971
4-life <pavel.ovchinnikov@reustle.co>
2022-04-25 22:06:29 +0200

    Show version in portfolio

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -2,12 +2,16 @@ import Footer from '../../components/Footer';
 import Head from '../../components/Head';
 import Portfolio from '../../components/Portfolio';
 
-export default () => {
+export default ({ version }) => {
   return (
     <div className="bg">
       <Head title="Portfolio" />
       <Portfolio />
-      <Footer />
+      <Footer version={version} />
     </div>
   );
 };
+
+export function getServerSideProps() {
+  return { props: { version: process.env.npm_package_version } };
+}
