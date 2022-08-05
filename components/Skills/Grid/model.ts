047646a61bd8dfb711fdff098ee70c9e3cc2840c
Pavel <4-life@users.noreply.github.com>
2022-08-05 12:25:40 +0300

    Fix skill view (#23)
    * Add new company
    
    * Fix skill view
    
    * Update skills page
    
    Co-authored-by: 4-life <pavel.ovchinnikov@reustle.co>

--- components/Skills/Grid/model.ts
+++ components/Skills/Grid/model.ts
@@ -1,46 +1,68 @@
-/* eslint-disable prettier/prettier */
+type Xy = { x: number; y: number };
 
-const model = {
+export const themeDefault = 'default';
+export const themeWhite = 'white';
+
+export type SkillModel = {
+  id: string;
+  gridImage: string[];
+  copy: string;
+  story: string;
+  color: string;
+  textColor: string;
+  scale: number;
+  url: string;
+  theme?: typeof themeDefault | typeof themeWhite;
+  corners?: Xy[];
+  positionX?: number;
+  positionY?: number;
+  image?: HTMLImageElement;
+};
+
+const model: { content: SkillModel[]; layout: number[] } = {
   content: [
     {
       id: 'aws',
       gridImage: ['libs/aws.png'],
       copy: 'Microservices',
       story:
-        'I have expirence with Amazon Web Services',
+        'I have production expirence with Amazon Web Services such as S3, EC2, Lambda, Cognito, Amplify, IAM, SNS, RDS and Kinesis.\n\nAlso I had migration experience from a monolith to AWS microservices.\n\n In the most cases I use AWS CDK to deploy project',
       color: '#04273a',
       textColor: '#ffffff',
       scale: 0.7,
       url: 'aws.amazon.com/',
+      theme: themeDefault,
     },
     {
       id: 'ionic',
       gridImage: ['libs/ionic.png'],
       copy: 'Ionic',
       story:
-        'I have expirence with the beautiful, open source front-end SDK for developing hybrid mobile apps with HTML5',
+        'I developed two hybrid mobile apps for Android. Apps use Google Maps for positioning sensors. I added code-push to deploy updates.\n\nOne app released on Google Play',
       color: '#dddddd',
       textColor: '#4e8ef7',
       scale: 0.7,
       url: 'ionicframework.com/',
+      theme: 'white',
     },
     {
       id: 'js',
       gridImage: ['libs/js.png'],
       copy: 'JavaScript',
       story:
-        'I have experienced with the last features of JS (ES6), async/await etc.',
+        'I use the last features of JS (ES6) such as DOM manipulating (without jQuery), async/await, modules etc.\n\nI know dynamic typing, how a garbage collector works, how to profile code in a browser console etc',
       color: '#f1ee00',
       textColor: '#000000',
       scale: 0.7,
       url: 'tc39.es/',
+      theme: 'white',
     },
     {
       id: 'postgres',
       gridImage: ['libs/postgres.png'],
       copy: 'PostgresSQL',
       story:
-        'I have expirence with PostgresSQL. TypeORM, pgadmin4, pg-promise',
+        'The most of my projects use PostgresSQL database. I use pgadmin4 for manual access to DB, simple library pg-promise for NodeJS and popular ORMs: TypeORM and Sequelize',
       color: '#336791',
       textColor: '#ffffff',
       scale: 0.7,
@@ -50,7 +72,7 @@ const model = {
       id: 'angular',
       gridImage: ['libs/angular.png'],
       copy: 'AngularJS',
-      story: 'I have expirence with AngularJS',
+      story: 'I have some projects on AngularJS, for example Ionic apps use AngularJS',
       color: '#b4b4b4',
       textColor: '#c4473a',
       scale: 0.7,
@@ -61,7 +83,7 @@ const model = {
       gridImage: ['libs/nodejs.png'],
       copy: 'NodeJS',
       story:
-        "Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices",
+        'I have some projects on NodeJS. Some of this project use Express or NestJS. In some cases I use NodeJS in AWS Lambda as RestApi service',
       color: '#303030',
       textColor: '#ffffff',
       scale: 0.7,
@@ -71,9 +93,8 @@ const model = {
       id: 'github',
       gridImage: ['libs/github.png'],
       copy: 'CI/CD',
-      story:
-        'I experienced with Git and Github Actions',
-      color: '#256edf',
+      story: 'I always try to use CI/CD automation such as Github Actions or Bitbucket Pipeline',
+      color: '#002f67',
       textColor: '#ffffff',
       scale: 0.7,
       url: 'github.com/',
@@ -82,7 +103,7 @@ const model = {
       id: 'react',
       gridImage: ['libs/react.png'],
       copy: 'React',
-      story: 'A javascript library for building user interfaces',
+      story: 'The last 3 years I use React on a front-end',
       color: '#222',
       textColor: '#00d8ff',
       scale: 0.7,
@@ -92,18 +113,18 @@ const model = {
       id: 'nextjs',
       gridImage: ['libs/next.png'],
       copy: 'NextJS',
-      story: 'I have expirenece with NextJS',
-      color: '#5ecdc8',
-      textColor: '#000000',
+      story: 'Current website based on NextJS',
+      color: '#fff',
+      textColor: '#333',
       scale: 0.7,
       url: 'nextjs.org/',
+      theme: 'white',
     },
     {
       id: 'redux',
       gridImage: ['libs/redux.png'],
       copy: 'State Management',
-      story:
-        'I experienced with redux+saga',
+      story: 'For a state management system in the most cases I use Saga',
       color: '#555555',
       textColor: '#999999',
       scale: 0.7,
@@ -114,17 +135,19 @@ const model = {
       gridImage: ['libs/mocha.png'],
       copy: 'Testing',
       story:
-        'I have experienced with tisting by Mocha + Chai',
-      color: '#c4473a',
-      textColor: '#ffffff',
+        'I always try to write code with tests. I use TestCafe for e2e testing and Mocha + Chai for unit and integrational testing',
+      color: '#ffffff',
+      textColor: '#333333',
       scale: 0.7,
       url: 'mochajs.org/',
+      theme: 'white',
     },
     {
       id: 'typescript',
       gridImage: ['libs/typescript.png'],
       copy: 'Typescript',
-      story: 'I have experienced with Typescript and positive in implementing strict types',
+      story:
+        "I can't imagine Javascript code without typing sysytem. A lot of bugs can be avoided just using strict types. I use Typescript in any kind of project",
       color: '#007acc',
       textColor: '#ffffff',
       scale: 0.7,
@@ -134,7 +157,7 @@ const model = {
       id: 'vue',
       gridImage: ['libs/vue.png'],
       copy: 'Vue',
-      story: 'I have experienced with Vue',
+      story: 'I created one production website using Vue',
       color: '#cccccc',
       textColor: '#435466',
       scale: 0.7,
@@ -142,6 +165,7 @@ const model = {
     },
   ],
   layout: [
+    // eslint-disable-next-line prettier/prettier
     143, 142, 141, 140, 139, 138, 137, 136, 135, 134, 133, 132,
     100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 131,
     101, 64, 63, 62, 61, 60, 59, 58, 57, 56, 89, 130,
@@ -154,16 +178,7 @@ const model = {
     108, 71, 42, 43, 44, 45, 46, 47, 48, 49, 82, 123,
     109, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 122,
     110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121
-  ]
-};
-
-type Xy = { x: number; y: number };
-
-export type SkillModel = typeof model.content[0] & {
-  corners?: Xy[];
-  positionX?: number;
-  positionY?: number;
-  image?: HTMLImageElement;
+  ],
 };
 
 export default model;
