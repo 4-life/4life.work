bc6b19bd0045a358ea6cb50593da4ff961f219af
Pavel Ovchinnikov <4-life@users.noreply.github.com>
2022-10-01 00:10:02 +0300

    Add CSP (#25)
    * Add CSP
    
    * Fix CSP
    
    * Fix links
    
    Co-authored-by: 4-life <pavel.ovchinnikov@reustle.co>

--- next.config.js
+++ next.config.js
@@ -1,5 +1,45 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'unsafe-inline' 'self';
+  child-src 'self';
+  style-src 'self' 'unsafe-inline';
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  connect-src 'self';
+  frame-src 'self';
+  img-src 'self' data:;
+  manifest-src 'self';
+  media-src 'self';
+  worker-src 'none';
+`;
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // eslint-disable-next-line require-await
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
   reactStrictMode: true,
   publicRuntimeConfig: {
     version: process.env.npm_package_version,
