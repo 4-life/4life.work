335a92e69120250e2e2997c493b60e9a546a57e0
Pavel <pavel.ovchinnikov@kupsilla.com>
2026-03-02 12:38:26 +0500

    Make skills page as a main page. Fix info

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Skills from '@/components/Skills';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Skills',
+  title: 'About me',
 };
 
 export default (): JSX.Element => {
