6649fde3c53b4315ab1c3b9b730699ec8c5f3d5c
Pavel <pavel.ovchinnikov@kupsilla.com>
2026-03-06 12:58:54 +0500

    Fix inline style CSP

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export function middleware(request: NextRequest): NextResponse {
   const csp = [
     `default-src 'self'`,
     `script-src 'self' 'nonce-${nonce}'`,
-    `style-src 'self' 'nonce-${nonce}' 'unsafe-inline'`,
+    `style-src 'self' 'unsafe-inline'`,
     `img-src 'self' blob: data:`,
     `font-src 'self'`,
     `object-src 'none'`,
